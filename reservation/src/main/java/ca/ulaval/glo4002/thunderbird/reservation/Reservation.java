5dfa00e205bdb4f389ceaed3f5272bbb4e15b635
Alexandre Rivest <rivest123@hotmail.com>
2016-09-18 19:44:18 -0500

    Add last attribute to reservation

--- reservation/src/main/java/ca/ulaval/glo4002/thunderbird/reservation/Reservation.java
+++ reservation/src/main/java/ca/ulaval/glo4002/thunderbird/reservation/Reservation.java
@@ -1,20 +1,32 @@
 package ca.ulaval.glo4002.thunderbird.reservation;
+import ca.ulaval.glo4002.thunderbird.boarding.Passenger;
+
+import java.util.ArrayList;
 
 public class Reservation {
 
     private int reservationNumber;
     private String reservationDate;
     private String reservationConfirmation;
+    private String flightNumber;
+    private String flightDate;
+    private ArrayList<Passenger> passengers;
     private String paymentLocation;
 
     public Reservation(int reservation_number,
                        String reservation_date,
                        String reservation_confirmation,
-                       String paymentLocation) {
+                       String flightNumber,
+                       String flightDate,
+                       String paymentLocation,
+                       ArrayList<Passenger> passengers) {
         this.reservationNumber = reservation_number;
         this.reservationDate = reservation_date;
         this.reservationConfirmation = reservation_confirmation;
         this.paymentLocation = paymentLocation;
+        this.flightDate = flightDate;
+        this.flightNumber = flightNumber;
+        this.passengers = new ArrayList<>(passengers);
     }
 
     public int getReservationNumber() {
@@ -31,5 +43,17 @@ public class Reservation {
 
     public String getPaymentLocation() {
         return paymentLocation;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getFlightDate() {
+        return flightDate;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
     }
 }
--- reservation/src/test/java/ca/ulaval/glo4002/thunderbird/reservation/ReservationTest.java
+++ reservation/src/test/java/ca/ulaval/glo4002/thunderbird/reservation/ReservationTest.java
@@ -1,7 +1,11 @@
 package ca.ulaval.glo4002.thunderbird.reservation;
 
+import ca.ulaval.glo4002.thunderbird.boarding.Passenger;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.jar.Pack200;
 
 import static org.junit.Assert.*;
 public class ReservationTest {
@@ -10,18 +14,30 @@ public class ReservationTest {
     private String RESERVATION_DATE = "2016-01-31";
     private String RESERVATION_CONFIRMATION = "A3833";
     private String PAYMENT_LOCATION = "/payments/da39a3ee5e6b4b0d3255bfef95601890afd80709";
+    private String FLIGHT_NUMBER = "AC1765";
+    private String FLIGHT_DATE = "2016-10-30";
+    private ArrayList<Passenger> PASSENGERS = new ArrayList<>();
     private Reservation newReservation;
 
     @Before
     public void newReservation() {
-        newReservation = new Reservation(RESERVATION_NUMBER, RESERVATION_DATE, RESERVATION_CONFIRMATION, PAYMENT_LOCATION);
+        newReservation = new Reservation(RESERVATION_NUMBER,
+                RESERVATION_DATE,
+                RESERVATION_CONFIRMATION,
+                FLIGHT_NUMBER,
+                FLIGHT_DATE,
+                PAYMENT_LOCATION,
+                PASSENGERS);
     }
 
     @Test
-    public void whenCreatingReservation_ShouldHaveNumberAndDateAndConfirmation() {
+    public void whenCreatingReservation_shouldReservationFieldsAreNotNull() {
         assertEquals(RESERVATION_NUMBER, newReservation.getReservationNumber());
         assertEquals(RESERVATION_DATE, newReservation.getReservationDate());
         assertEquals(RESERVATION_CONFIRMATION, newReservation.getReservationConfirmation());
         assertEquals(PAYMENT_LOCATION, newReservation.getPaymentLocation());
+        assertEquals(FLIGHT_DATE, newReservation.getFlightDate());
+        assertEquals(FLIGHT_NUMBER, newReservation.getFlightNumber());
+        assertEquals(PASSENGERS, newReservation.getPassengers());
     }
 }
