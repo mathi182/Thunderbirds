5cc799197265f379b4067e81c3674ee344f6eae0
Jean-Alexandre Beaumont <jean-alexandre.beaumont.1@ulaval.ca>
2016-10-06 12:31:03 -0400

    FlightRepository completed. Used to get the PlaneModel when assigning a seat.

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/domain/FlightRepository.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/domain/FlightRepository.java
@@ -1,27 +1,51 @@
 package ca.ulaval.glo4002.thunderbird.boarding.domain;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import javax.ws.rs.NotFoundException;
 import java.util.HashMap;
+import java.util.Random;
 
 public class FlightRepository {
-        private static HashMap<String,String> flightRepository = new HashMap();
+
+    private static final String DASH_FLIGHT_NUMBER = "QK-918";
+    private static final String A320_FLIGHT_NUMBER = "NK-750";
+    private static final String BOEING_FLIGHT_NUMBER = "QK-432";
+    private static final String DASH_MODEL = "dash-8";
+    private static final String A320_MODEL = "a320";
+    private static final String BOEING_MODEL = "boeing-777-300";
+
+    private Random generator = new Random();
+    private static HashMap<String,String> flightRepository = new HashMap();
     private String[] modelSet = {"dash-8","a320","boeing-777-300"};
 
-    public static void addFlight(String flightNumber) {
-        //TODO Check if the flight was already registered before updating
-        String planeModel = ""; //TODO Randomly select a plane model
-        flightRepository.put(flightNumber,planeModel);
+    public void addFlight(String flightNumber) {
+        if (flightRepository.containsKey(flightNumber)) {
+            //Do nothing (Should we throw something?)
+        }
+        else {
+            String planeModel = modelSet[generator.nextInt(3)];
+            flightRepository.put(flightNumber,planeModel);
+        }
     }
 
-    public static String getPlaneModel(String flightNumber) {
-        try {
+    public String getPlaneModel(String flightNumber) {
             return flightRepository.get(flightNumber);
-        }
-        catch (NotFoundException e) {
-            throw new NotImplementedException(); //TODO Implement a Flight not found exception
-        }
+    }
+
+    public boolean contains(String flight_number) {
+        return flightRepository.containsKey(flight_number);
+    }
+
+    public void clear() {
+        flightRepository.clear();
+    }
+
+    public int size() {
+        return flightRepository.size();
+    }
+
+    public void demoSetup() {
+        flightRepository.put(BOEING_FLIGHT_NUMBER,BOEING_MODEL);
+        flightRepository.put(DASH_FLIGHT_NUMBER,DASH_MODEL);
+        flightRepository.put(A320_FLIGHT_NUMBER,A320_MODEL);
     }
 }
 
--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/domain/FlightRepositoryTest.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/domain/FlightRepositoryTest.java
@@ -1,18 +1,66 @@
 package ca.ulaval.glo4002.thunderbird.boarding.domain;
 
+import org.junit.After;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class FlightRepositoryTest {
 
-    FlightRepository repository = new FlightRepository();
-    String FLIGHT_NUMBER = "RD3213";
+    private static final FlightRepository repository = new FlightRepository();
+    private static final String NEW_FLIGHT_NUMBER = "RD3213";
+    private static final String DASH_FLIGHT_NUMBER = "QK-918";
+    private static final String A320_FLIGHT_NUMBER = "NK-750";
+    private static final String BOEING_FLIGHT_NUMBER = "QK-432";
+    private static final String BOEING_MODEL = "boeing-777-300";
+    private static final String DASH_MODEL = "dash-8";
+    private static final String A320_MODEL = "a320";
+
+    @After
+    public void setUp() {
+        repository.clear();
+    }
 
     @Test
-    public void givenANewFlightNumber_whenUpdatingTheFlightRepository_shouldAPlaneModelIsAssigned() {
-        //repository.addFlight(FLIGHT_NUMBER);
-        assertTrue(true); //TODO REMOVE ME!
+    public void givenANewFlightNumber_whenAddingToTheFlightRepository_shouldContainTheFlight() {
+        repository.addFlight(NEW_FLIGHT_NUMBER);
+        assertTrue(repository.contains(NEW_FLIGHT_NUMBER));
     }
+
+    @Test
+    public void givenANewFlightNumber_whenAddingToTheFlightRepository_shouldHaveAPlaneModel() {
+        repository.addFlight(NEW_FLIGHT_NUMBER);
+        assertNotNull(repository.getPlaneModel(NEW_FLIGHT_NUMBER));
+    }
+
+    @Test
+    public void givenANewFlightNumber_whenRetrievingTheModel_shouldReturnNull() {
+        String actualResult = repository.getPlaneModel(NEW_FLIGHT_NUMBER);
+
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void givenANonEmptyRepository_whenClearingTheRepository_shouldBeEmpty() {
+        repository.addFlight(NEW_FLIGHT_NUMBER);
+
+        repository.clear();
+
+        assertTrue(repository.size() == 0);
+    }
+
+    @Test
+    public void whenUsingDemoSetup_shouldHaveSpecificFlightModelInRepository() {
+        repository.demoSetup();
+
+        assertTrue(repository.contains(BOEING_FLIGHT_NUMBER));
+        assertTrue(repository.contains(DASH_FLIGHT_NUMBER));
+        assertTrue(repository.contains(A320_FLIGHT_NUMBER));
+        assertEquals(repository.getPlaneModel(BOEING_FLIGHT_NUMBER),BOEING_MODEL);
+        assertEquals(repository.getPlaneModel(DASH_FLIGHT_NUMBER),DASH_MODEL);
+        assertEquals(repository.getPlaneModel(A320_FLIGHT_NUMBER),A320_MODEL);
+    }
+
+
 
 }
