22801d360debe41121a82a157a7d80e195c527eb
Alexis Lessard <alexislessard95@gmail.com>
2016-11-04 15:13:16 -0400

    seperated seatAssignationResource into multiple functions

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/SeatAssignationsResource.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/SeatAssignationsResource.java
@@ -33,16 +33,13 @@ public class SeatAssignationsResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response assignSeat(SeatAssignationRequest request) {
-        Passenger passenger = new SeatAssignationRequestAssembler().getDomainPassenger(request);
-        Flight flight = repository.getFlight(passenger.getFlightNumber(), passenger.getFlightDate());
-        SeatAssignationStrategy.assignMode assignMode = new SeatAssignationRequestAssembler().getMode(request);
-        SeatAssignationStrategy strategy = new SeatAssignationStrategyFactory().getStrategy(assignMode);
+        Flight flight = getFlight(request);
+        SeatAssignationStrategy strategy = getSeatAssignationStrategy(request);
 
         Seat seat = flight.assignSeat(strategy);
         seat.take();
-        
-        TakenSeatAssembler assembler = new TakenSeatAssembler();
-        TakenSeatDTO takenSeatDTO = assembler.fromDomain(seat);
+
+        TakenSeatDTO takenSeatDTO = convertSeatToDTO(seat);
 
         String seatAssignationsIdString = String.valueOf(new Random().nextInt());
         UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
@@ -50,5 +47,25 @@ public class SeatAssignationsResource {
         return Response.created(uri)
                 .entity(takenSeatDTO)
                 .build();
+    }
+
+    private Flight getFlight(SeatAssignationRequest request){
+        SeatAssignationRequestAssembler seatAssignationRequestAssembler = new SeatAssignationRequestAssembler();
+        Passenger passenger = seatAssignationRequestAssembler.getDomainPassenger(request);
+        Flight flight = repository.getFlight(passenger.getFlightNumber(), passenger.getFlightDate());
+        return flight;
+    }
+
+    private SeatAssignationStrategy getSeatAssignationStrategy(SeatAssignationRequest request){
+        SeatAssignationRequestAssembler seatAssignationRequestAssembler = new SeatAssignationRequestAssembler();
+        SeatAssignationStrategy.assignMode assignMode = seatAssignationRequestAssembler.getMode(request);
+        SeatAssignationStrategy strategy = new SeatAssignationStrategyFactory().getStrategy(assignMode);
+        return strategy;
+    }
+
+    private TakenSeatDTO convertSeatToDTO(Seat seat){
+        TakenSeatAssembler assembler = new TakenSeatAssembler();
+        TakenSeatDTO takenSeatDTO = assembler.fromDomain(seat);
+        return takenSeatDTO;
     }
 }
