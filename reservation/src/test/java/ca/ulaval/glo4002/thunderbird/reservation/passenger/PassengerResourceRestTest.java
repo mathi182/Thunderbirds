3bb73bb10cf48c7af25c337f77b9af6ecfd86a4a
Alexis Lessard <alexislessard95@gmail.com>
2016-11-05 17:08:08 -0400

    Created and tested Passenger Resource, without REST test. Removed PassengerRequest

--- reservation/src/test/java/ca/ulaval/glo4002/thunderbird/reservation/passenger/PassengerResourceRestTest.java
+++ reservation/src/test/java/ca/ulaval/glo4002/thunderbird/reservation/passenger/PassengerResourceRestTest.java
@@ -1,8 +1,22 @@
+package ca.ulaval.glo4002.thunderbird.reservation.passenger;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static ca.ulaval.glo4002.thunderbird.reservation.RestTestConfig.givenBaseRequest;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.OK;
 import static org.junit.Assert.*;
 
-/**
- * Created by AlexisLessard on 2016-11-05.
- */
-public class PassengerResourceTest {
+
+public class PassengerResourceRestTest {
+    @Test
+    public void givenNoPassengerNumber_WhenAskingForPassenger_ShouldReturnBadRequest(){
+        givenBaseRequest()
+                .when()
+                .get("/passenger/test")
+                .then()
+                .statusCode(BAD_REQUEST.getStatusCode());
+    }
 
 }
