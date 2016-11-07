bb3e6f498fb5e9145842692352860adfac87e772
Alexis Lessard <alexislessard95@gmail.com>
2016-11-07 18:02:07 -0500

    added and tested PassengerAssembler

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/Passenger/PassengerAssembler.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/Passenger/PassengerAssembler.java
@@ -1,5 +1,30 @@
 package ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger;
 
 
+import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.Passenger;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.plane.Seat;
+
+import java.util.UUID;
+
 public class PassengerAssembler {
+
+    private static final String ECONOMY = "economy";
+    private static final Seat.SeatClass DEFAULT_SEAT_CLASS = Seat.SeatClass.ANY;
+
+    public Passenger toDomain(PassengerRequest passengerRequest){
+        Seat.SeatClass seatClass = getSeatClassFromString(passengerRequest.seatClass);
+        UUID passengerHash = UUID.fromString(passengerRequest.passengerHash);
+
+        return new Passenger(passengerHash,seatClass);
+    }
+
+    private Seat.SeatClass getSeatClassFromString(String source){
+        Seat.SeatClass seatClass = DEFAULT_SEAT_CLASS;
+        switch (source){
+            case ECONOMY:
+                seatClass = Seat.SeatClass.ECONOMY;
+        }
+
+        return seatClass;
+    }
 }
--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/Passenger/PassengerAssemblerTest.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/Passenger/PassengerAssemblerTest.java
@@ -1,9 +1,28 @@
 package ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger;
 
+import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.Passenger;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.plane.Seat;
 import org.junit.Test;
+
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
 public class PassengerAssemblerTest {
+    private static final UUID VALID_PASSENGER_HASH = UUID.randomUUID();
+    private static final Seat.SeatClass VALID_PASSENGER_SEAT_CLASS = Seat.SeatClass.ECONOMY;
+    private static final String ECONOMY = "economy";
 
+    @Test
+    public void givenFilledPassengerRequest_whenTransformingToDomain_ShouldBeTheCorrectPassenger(){
+        PassengerRequest passengerRequest = new PassengerRequest(VALID_PASSENGER_HASH.toString(),ECONOMY);
+
+        Passenger actualPassenger = new PassengerAssembler().toDomain(passengerRequest);
+        UUID actualPassengerHash = actualPassenger.getHash();
+        boolean isTheSameSeatClass = actualPassenger.isSameSeatClass(VALID_PASSENGER_SEAT_CLASS);
+
+        UUID expectedPassengerHash = VALID_PASSENGER_HASH;
+        assertEquals(expectedPassengerHash,actualPassengerHash);
+        assertTrue(isTheSameSeatClass);
+    }
 }
