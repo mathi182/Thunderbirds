cb08a387d1753da3908708f80b61571d379a0d5c
Alexis Lessard <alexislessard95@gmail.com>
2016-11-08 17:47:22 -0500

    added PassengerAPICaller to take care of API calls. Modified constructor of PAssengerFetcher and tests

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/Passenger/PassengerFetcher.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/Passenger/PassengerFetcher.java
@@ -12,8 +12,14 @@ import java.util.UUID;
 import static com.sun.jersey.api.client.ClientResponse.Status.OK;
 
 public class PassengerFetcher {
-    public static final String URL = "http://127.0.0.1:8888";
-    public static final String SERVICE_PATH_FORMAT = "/passenger/%1s";
+
+    private PassengerAssembler passengerAssembler;
+    private PassengerAPICaller apiCaller;
+
+    public PassengerFetcher(PassengerAssembler passengerAssembler, PassengerAPICaller apiCaller){
+        this.passengerAssembler = passengerAssembler;
+        this.apiCaller = apiCaller;
+    }
 
     public Passenger fetchPassenger(UUID PassengerHash) {
         PassengerRequest request = getPassengerRequestFromAPI(PassengerHash);
@@ -21,27 +27,18 @@ public class PassengerFetcher {
         return passenger;
     }
 
-    private PassengerRequest getPassengerRequestFromAPI(UUID passengerHash){
-        String url = URL + String.format(SERVICE_PATH_FORMAT, passengerHash);;
-        ClientResponse response = getResource(url);
-        validateResponse(response,passengerHash);
+    private PassengerRequest getPassengerRequestFromAPI(UUID passengerHash) {
+        ClientResponse response = apiCaller.requestPassenger(passengerHash.toString());
+        validateResponse(response, passengerHash);
         return response.getEntity(PassengerRequest.class);
     }
-    private ClientResponse getResource(String url) {
-        return Client
-                .create()
-                .resource(url)
-                .accept(MediaType.APPLICATION_JSON)
-                .header("content-type", MediaType.APPLICATION_JSON)
-                .get(ClientResponse.class);
-    }
 
-    private void validateResponse(ClientResponse response, UUID passengerHash){
-        if(response.getStatus() != OK.getStatusCode())
+    private void validateResponse(ClientResponse response, UUID passengerHash) {
+        if (response.getStatus() != OK.getStatusCode())
             throw new PassengerNotFoundException(passengerHash);
     }
-    private Passenger getPassengerFromRequest(PassengerRequest request){
-        return new PassengerAssembler().toDomain(request);
-    }
 
+    private Passenger getPassengerFromRequest(PassengerRequest request) {
+        return passengerAssembler.toDomain(request);
+    }
 }
--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/Passenger/PassengerFetcherTest.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/Passenger/PassengerFetcherTest.java
@@ -2,39 +2,72 @@ package ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger;
 
 import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.Passenger;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.exceptions.PassengerNotFoundException;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
+import org.mockito.Mockito;
 
 
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
 import java.util.UUID;
 
+import static com.sun.jersey.api.client.ClientResponse.Status.NOT_FOUND;
+import static com.sun.jersey.api.client.ClientResponse.Status.OK;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
 
 public class PassengerFetcherTest{
-    private static final UUID VALID_PASSENGER_HASH = UUID.fromString("3b99dbde-7fe9-4cbe-91bf-72174ea0d829");
+    private static final UUID VALID_PASSENGER_HASH = UUID.randomUUID();
     private static final UUID RANDOM_UUID = UUID.randomUUID();
+
+    private PassengerRequest passengerRequestMock;
+    private PassengerAssembler passengerAssemblerMock;
+    private PassengerAPICaller passengerAPIcallerMock;
+    private ClientResponse clientResponseMock;
+    private Passenger passengerMock;
+
 
     private PassengerFetcher passengerFetcherTest;
 
+
     @Before
     public void setup(){
-        passengerFetcherTest = new PassengerFetcher();
+        passengerAssemblerMock = mock(PassengerAssembler.class);
+        passengerRequestMock = mock(PassengerRequest.class);
+        passengerAPIcallerMock = mock(PassengerAPICaller.class);
+        clientResponseMock = mock(ClientResponse.class);
+
+        willReturn(clientResponseMock).given(passengerAPIcallerMock).requestPassenger(anyString());
+        willReturn(passengerRequestMock).given(clientResponseMock).getEntity(PassengerRequest.class);
+        willReturn(passengerMock).given(passengerAssemblerMock).toDomain(passengerRequestMock);
+
+        passengerFetcherTest = new PassengerFetcher(passengerAssemblerMock,passengerAPIcallerMock);
     }
 
+
+
     @Test
-    @Ignore //not a unit test, need to start reservation server for it to work
     public void givenNewPassengerFetcher_whenRequestingValidPassenger_ShouldBeCorrectPassenger(){
+        willReturn(OK.getStatusCode()).given(clientResponseMock).getStatus();
+
         Passenger passenger = passengerFetcherTest.fetchPassenger(VALID_PASSENGER_HASH);
 
-        assertEquals(VALID_PASSENGER_HASH,passenger.getHash());
+        assertEquals(passengerMock,passenger);
     }
 
     @Test(expected = PassengerNotFoundException.class)
-    @Ignore //not a unit test, need to start reservation server for it to work
     public void givenNewPassengerFetcher_whenRequestingExistingPassenger_ShouldThrowPassengerNotFound(){
+        willReturn(NOT_FOUND.getStatusCode()).given(clientResponseMock).getStatus();
+
         passengerFetcherTest.fetchPassenger(RANDOM_UUID);
     }
+
+
 }
