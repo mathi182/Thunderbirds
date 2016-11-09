01135495b81006982f65b1fee0eccaecbcd8a22a
Jean-Alexandre Beaumont <jean-alexandre.beaumont.1@ulaval.ca>
2016-11-08 20:26:34 -0500

    Added tests for HibernateRepo. Could not quickly fix the Passenger from reservation issue. It requires too much work (not just swapping) will do in another branch, keeping the TODOs

--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/persistence/passenger/HibernatePassengerRepositoryITest.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/persistence/passenger/HibernatePassengerRepositoryITest.java
@@ -8,6 +8,7 @@ import ca.ulaval.glo4002.thunderbird.boarding.persistence.EntityManagerProvider;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.exceptions.RepositorySavingException;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.exceptions.PassengerNotFoundException;
 import ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger.PassengerFetcher;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -22,12 +23,14 @@ import java.util.UUID;
 import static ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.LinearDimensionUnits.CM;
 import static ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.WeightUnits.KG;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.mock;
 
 
 public class HibernatePassengerRepositoryITest {
     private static final UUID VALID_PASSENGER_UUID = UUID.randomUUID();
+    private static final UUID VALID_PASSENGER_UUID_PRESENT_IN_RESERVATION = UUID.randomUUID();
     private static final UUID NON_EXISTENT_PASSENGER_UUID = UUID.randomUUID();
     private static final UUID PASSENGER_UUID_WITH_BAGGAGE = UUID.randomUUID();
     private PassengerRepository hibernatePassengerRepository;
@@ -44,33 +47,49 @@ public class HibernatePassengerRepositoryITest {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityManagerProvider.setEntityManager(entityManager);
         hibernatePassengerRepository = new HibernatePassengerRepositoryImpl(passengerFetcher);
+    }
 
+    @After
+    public void tearDown() {
     }
 
     @Test(expected = PassengerNotFoundException.class)
     public void givenEmptyRepository_whenGettingPassenger_shouldThrowException() {
         willThrow(new PassengerNotFoundException(NON_EXISTENT_PASSENGER_UUID))
                 .given(passengerFetcher).fetchPassenger(NON_EXISTENT_PASSENGER_UUID);
+
         hibernatePassengerRepository.getPassenger(NON_EXISTENT_PASSENGER_UUID);
     }
 
     @Test
     public void givenEmptyRepository_whenSavingPassenger_shouldBeSavedCorrectly() throws RepositorySavingException {
         Passenger expectedPassenger = new Passenger(VALID_PASSENGER_UUID, Seat.SeatClass.ANY);
+
         hibernatePassengerRepository.savePassenger(expectedPassenger);
         Passenger actualPassenger = hibernatePassengerRepository.getPassenger(VALID_PASSENGER_UUID);
+
         assertEquals(expectedPassenger, actualPassenger);
     }
 
-    //TODO Ajouter un test pour si le passager n'est pas dans la BD de boarding mais présent dans celle de reservation
+    @Test
+    public void givenEmptyRepository_whenGettingAPassengerPresentInReservation_shouldReturnThePassenger() {
+        Passenger expectedPassenger = new Passenger(VALID_PASSENGER_UUID_PRESENT_IN_RESERVATION,Seat.SeatClass.ANY);
+        willReturn(expectedPassenger).given(passengerFetcher).fetchPassenger(VALID_PASSENGER_UUID_PRESENT_IN_RESERVATION);
+
+        Passenger actualPassenger = hibernatePassengerRepository.getPassenger(VALID_PASSENGER_UUID_PRESENT_IN_RESERVATION);
+
+        assertEquals(expectedPassenger,actualPassenger);
+    }
 
     @Test
     public void givenEmptyRepository_whenSavingPassengerWithBaggages_shouldSaveBaggageCorrectly() throws RepositorySavingException {
         Baggage baggage = new CheckedBaggageEconomy(CM, 10, KG, 10);
         List<Baggage> baggageList = Arrays.asList(baggage);
         Passenger expectedPassenger = new Passenger(PASSENGER_UUID_WITH_BAGGAGE, Seat.SeatClass.ANY, baggageList);
+
         hibernatePassengerRepository.savePassenger(expectedPassenger);
         Passenger actualPassenger = hibernatePassengerRepository.getPassenger(PASSENGER_UUID_WITH_BAGGAGE);
+
         assertEquals(expectedPassenger, actualPassenger);
     }
 }
