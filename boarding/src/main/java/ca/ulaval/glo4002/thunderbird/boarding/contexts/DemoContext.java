8f44b09be1ff99e54036a92d5b271e97d6fd2bad
Jean-Alexandre Beaumont <jean-alexandre.beaumont.1@ulaval.ca>
2016-11-09 23:40:31 -0500

    Changed the Demo context to insantiate the three flight required by the Demo.

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/contexts/DemoContext.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/contexts/DemoContext.java
@@ -2,32 +2,67 @@ package ca.ulaval.glo4002.thunderbird.boarding.contexts;
 
 import ca.ulaval.glo4002.thunderbird.boarding.domain.flight.AMSSystem;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.flight.AMSSystemFactory;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.flight.Flight;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.plane.Plane;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.plane.Seat;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.flight.FlightRepository;
-import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.HibernatePassengerRepository;
-import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerRepository;
-import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerRepositoryProvider;
-import ca.ulaval.glo4002.thunderbird.boarding.persistence.plane.PlaneRepository;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.flight.FlightRepositoryProvider;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.flight.HibernateFlightRepository;
-import ca.ulaval.glo4002.thunderbird.boarding.persistence.plane.PlaneRepositoryProvider;
-import ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger.PassengerAPICaller;
-import ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger.PassengerAssembler;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.HibernatePassengerRepository;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerFetcher;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerRepository;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerRepositoryProvider;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.plane.PlaneService;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.plane.PlaneServiceGlo3000;
+import ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger.PassengerAssembler;
+import ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger.PassengerService;
+
+import java.time.Instant;
+import java.util.List;
 
 public class DemoContext implements Context {
     @Override
     public void apply() {
+        registerFlightRepository();
+        registerPassengerRepository();
+    }
+
+    private FlightRepository registerFlightRepository() {
         AMSSystemFactory amsSystemFactory = new AMSSystemFactory();
         AMSSystem amsSystem = amsSystemFactory.create();
-        PlaneRepositoryProvider planeRepositoryProvider = new PlaneRepositoryProvider();
-        PlaneRepository planeRepository = planeRepositoryProvider.getRepository();
-        FlightRepository flightRepository = new HibernateFlightRepository(amsSystem, planeRepository);
+        PlaneService planeService = new PlaneServiceGlo3000();
+        FlightRepository flightRepository = new HibernateFlightRepository(amsSystem, planeService);
         new FlightRepositoryProvider().setFlightRepository(flightRepository);
-
-        PassengerAPICaller apiCaller = new PassengerAPICaller();
+        initializeDefaultFlights(flightRepository, amsSystem, planeService);
+        return flightRepository;
+    }
+    private PassengerRepository registerPassengerRepository() {
+        PassengerService apiCaller = new PassengerService();
         PassengerAssembler assembler = new PassengerAssembler();
         PassengerFetcher fetcher = new PassengerFetcher(assembler,apiCaller);
         PassengerRepository passengerRepository = new HibernatePassengerRepository(fetcher);
         new PassengerRepositoryProvider().setPassengerRepository(passengerRepository);
+        return passengerRepository;
+    }
+
+    private void initializeDefaultFlights(FlightRepository flightRepository, AMSSystem amsSystem, PlaneService planeService) {
+        String dash8FlightNumber = "QK-918";
+        String a320FlightNumber = "NK-750";
+        String boeingFlightNumber = "QK-432";
+
+        Flight qk918Flight = getFlight(amsSystem, planeService, dash8FlightNumber);
+        Flight nk750Flight = getFlight(amsSystem,planeService,a320FlightNumber);
+        Flight qk432Flight = getFlight(amsSystem, planeService, boeingFlightNumber);
+
+        flightRepository.saveFlight(qk918Flight);
+        flightRepository.saveFlight(nk750Flight);
+        flightRepository.saveFlight(qk432Flight);
+    }
+
+    private Flight getFlight(AMSSystem amsSystem, PlaneService planeService, String flightNumber) {
+        String dash8Model = amsSystem.getPlaneModel(flightNumber);
+        Plane dash8Plane = planeService.getPlaneInformation(dash8Model);
+        List<Seat> dash8Seats = planeService.getSeats(dash8Model);
+        return new Flight(flightNumber, Instant.now(),dash8Plane,dash8Seats);
     }
 }
--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/contexts/DevContext.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/contexts/DevContext.java
@@ -6,25 +6,24 @@ import ca.ulaval.glo4002.thunderbird.boarding.persistence.flight.FlightRepositor
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.flight.FlightRepositoryProvider;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.flight.HibernateFlightRepository;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.HibernatePassengerRepository;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerFetcher;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerRepository;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerRepositoryProvider;
-import ca.ulaval.glo4002.thunderbird.boarding.persistence.plane.PlaneRepository;
-import ca.ulaval.glo4002.thunderbird.boarding.persistence.plane.PlaneRepositoryProvider;
-import ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger.PassengerAPICaller;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.plane.PlaneService;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.plane.PlaneServiceGlo3000;
 import ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger.PassengerAssembler;
-import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.PassengerFetcher;
+import ca.ulaval.glo4002.thunderbird.boarding.rest.Passenger.PassengerService;
 
 public class DevContext implements Context {
     @Override
     public void apply() {
         AMSSystemFactory amsSystemFactory = new AMSSystemFactory();
         AMSSystem amsSystem = amsSystemFactory.create();
-        PlaneRepositoryProvider planeRepositoryProvider = new PlaneRepositoryProvider();
-        PlaneRepository planeRepository = planeRepositoryProvider.getRepository();
-        FlightRepository flightRepository = new HibernateFlightRepository(amsSystem, planeRepository);
+        PlaneService planeService = new PlaneServiceGlo3000();
+        FlightRepository flightRepository = new HibernateFlightRepository(amsSystem, planeService);
         new FlightRepositoryProvider().setFlightRepository(flightRepository);
 
-        PassengerAPICaller apiCaller = new PassengerAPICaller();
+        PassengerService apiCaller = new PassengerService();
         PassengerAssembler assembler = new PassengerAssembler();
         PassengerFetcher fetcher = new PassengerFetcher(assembler,apiCaller);
         PassengerRepository passengerRepository = new HibernatePassengerRepository(fetcher);
