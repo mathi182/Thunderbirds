5caebaa370418922806994a6fccc631247defa15
Maxime Trottier <maxime.trottier@outlook.com>
2016-11-11 11:14:56 -0500

    Add comment on jersey properties

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/BoardingServer.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/BoardingServer.java
@@ -90,7 +90,9 @@ public class BoardingServer implements Runnable {
 
     private void configurerJersey(ServletContextHandler servletContextHandler) {
         ResourceConfig resourceConfig = new ResourceConfig().packages("ca.ulaval.glo4002.thunderbird.boarding")
+                // Now you can expect validation errors to be sent to the client.
                 .property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true)
+                // @ValidateOnExecution annotations on subclasses won't cause errors.
                 .property(ServerProperties.BV_DISABLE_VALIDATE_ON_EXECUTABLE_OVERRIDE_CHECK, true);
 
         ServletContainer container = new ServletContainer(resourceConfig);
--- reservation/src/main/java/ca/ulaval/glo4002/thunderbird/reservation/ReservationServer.java
+++ reservation/src/main/java/ca/ulaval/glo4002/thunderbird/reservation/ReservationServer.java
@@ -90,7 +90,9 @@ public class ReservationServer implements Runnable {
     private void configureJersey(ServletContextHandler servletContextHandler) {
         ResourceConfig resourceConfig = new ResourceConfig()
                 .packages("ca.ulaval.glo4002.thunderbird.reservation")
+                // Now you can expect validation errors to be sent to the client.
                 .property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true)
+                // @ValidateOnExecution annotations on subclasses won't cause errors.
                 .property(ServerProperties.BV_DISABLE_VALIDATE_ON_EXECUTABLE_OVERRIDE_CHECK, true);
 
         ServletContainer container = new ServletContainer(resourceConfig);
