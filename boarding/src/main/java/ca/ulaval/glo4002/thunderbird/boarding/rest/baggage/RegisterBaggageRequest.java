08c067fbf1371df384132551528c7e06e23ded46
alexbrillant <abrillant23@gmail.com>
2016-11-10 22:25:47 -0500

    refactored assembler

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/RegisterBaggageRequest.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/RegisterBaggageRequest.java
@@ -1,20 +1,27 @@
 package ca.ulaval.glo4002.thunderbird.boarding.rest.baggage;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.internal.NotNull;
 
 public class RegisterBaggageRequest {
+
+    @NotNull
     @JsonProperty("linear_dimension")
     public Integer linearDimension;
 
+    @NotNull
     @JsonProperty("linear_dimension_unit")
     public String linearDimensionUnit;
 
+    @NotNull
     @JsonProperty("weightInG")
     public Integer weight;
 
+    @NotNull
     @JsonProperty("weight_unit")
     public String weightUnit;
 
+    @NotNull
     @JsonProperty("type")
     public String type;
 
--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/RegisterBaggageRequestAssembler.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/RegisterBaggageRequestAssembler.java
@@ -10,8 +10,6 @@ import ca.ulaval.glo4002.thunderbird.boarding.util.Strings;
 
 public class RegisterBaggageRequestAssembler {
     public Baggage getDomainBaggage(RegisterBaggageRequest request) {
-        validateRequest(request);
-
         int dimension = convertRequestDimension(request);
         int weight = convertRequestWeight(request);
         String type = request.type;
@@ -22,36 +20,12 @@ public class RegisterBaggageRequestAssembler {
     private int convertRequestDimension(RegisterBaggageRequest request) {
         DimensionConverterFactory dimensionConverterFactory = new DimensionConverterFactory();
         DimensionConverter dimensionConverter = dimensionConverterFactory.getConverter(request.linearDimensionUnit);
-        int convertedDimension = dimensionConverter.convertToMillimeters(request.linearDimension);
-        return convertedDimension;
+        return dimensionConverter.convertToMillimeters(request.linearDimension);
     }
 
     private int convertRequestWeight(RegisterBaggageRequest request) {
         WeightConverterFactory weightConverterFactory = new WeightConverterFactory();
         WeightConverter weightConverter = weightConverterFactory.getConverter(request.weightUnit);
-        int convertedWeight = weightConverter.convertToGrams(request.weight);
-        return convertedWeight;
-    }
-
-    private void validateRequest(RegisterBaggageRequest request) {
-        if (Strings.isNullOrEmpty(request.linearDimensionUnit)) {
-            throw new MissingFieldException("linear_dimension_unit");
-        }
-
-        if (Strings.isNullOrEmpty(request.weightUnit)) {
-            throw new MissingFieldException("weight_unit");
-        }
-
-        if (Strings.isNullOrEmpty(request.type)) {
-            throw new MissingFieldException("type");
-        }
-
-        if (request.linearDimension == null) {
-            throw new MissingFieldException("linear_dimension");
-        }
-
-        if (request.weight == null) {
-            throw new MissingFieldException("weightInG");
-        }
+        return weightConverter.convertToGrams(request.weight);
     }
 }
