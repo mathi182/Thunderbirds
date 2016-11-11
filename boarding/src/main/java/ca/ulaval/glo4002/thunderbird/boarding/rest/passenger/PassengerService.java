0cce531d836f9162ec6d3484fec732c51f2605e9
Maxime Trottier <maxime.trottier@outlook.com>
2016-11-10 19:56:18 -0500

    Remove unused constant

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/passenger/PassengerService.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/passenger/PassengerService.java
@@ -1,24 +1,16 @@
 package ca.ulaval.glo4002.thunderbird.boarding.rest.passenger;
 
-
 import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.Passenger;
 import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.exceptions.PassengerNotFoundException;
-import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 
-import javax.ws.rs.core.MediaType;
 import java.util.UUID;
 
 import static com.sun.jersey.api.client.ClientResponse.Status.OK;
-import static com.sun.jersey.api.client.ClientResponse.Status.PARTIAL_CONTENT;
 
 public class PassengerService {
-
     private PassengerAssembler passengerAssembler;
     private PassengerRequest passengerRequest;
-    private static final String SERVICE_LOCATION = "http://127.0.0.1:8787";
-    private static final String SERVICE_PATH_FORMAT = "/passengers/%1s";
-
 
     public PassengerService(PassengerAssembler passengerAssembler, PassengerRequest passengerRequest){
         this.passengerAssembler = passengerAssembler;
