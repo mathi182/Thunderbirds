64f4ddc660c2eddb404510f3b169a2f1dbd5285f
Mathieu Boily <mathiboily@hotmail.com>
2016-11-11 22:41:50 -0500

    Story 8 done. A test is not perfect. Not able to make it work.

---
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/BaggageDTO.java
@@ -0,0 +1,7 @@
+package ca.ulaval.glo4002.thunderbird.boarding.rest.baggage;
+
+/**
+ * Created by mathi on 2016-11-11.
+ */
+public class BaggageDTO {
+}
---
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/BaggagesListDTO.java
@@ -0,0 +1,10 @@
+package ca.ulaval.glo4002.thunderbird.boarding.rest.baggage;
+
+import ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.Baggage;
+
+import java.util.List;
+
+public class BaggagesDTO {
+    public float totalPrice;
+    public List<Baggage> baggages;
+}
