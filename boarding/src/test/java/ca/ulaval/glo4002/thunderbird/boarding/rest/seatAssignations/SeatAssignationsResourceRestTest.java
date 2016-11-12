fe14a7194d995c1121d51670df8bdbfc20868fd0
mr0ar <myriam.moar@gmail.com>
2016-11-11 19:59:02 -0500

    adding some rest test to seatAssignations

--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/seatAssignations/SeatAssignationsResourceRestTest.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/seatAssignations/SeatAssignationsResourceRestTest.java
@@ -1,25 +1,20 @@
 package ca.ulaval.glo4002.thunderbird.boarding.rest.seatAssignations;
 
-import ca.ulaval.glo4002.thunderbird.boarding.domain.flight.Flight;
-import ca.ulaval.glo4002.thunderbird.boarding.domain.seatAssignations.SeatAssignationStrategy;
-import ca.ulaval.glo4002.thunderbird.boarding.domain.seatAssignations.exceptions.SeatNotAvailableException;
 import io.restassured.response.Response;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.util.UUID;
+import java.util.regex.Pattern;
 
+import static ca.ulaval.glo4002.thunderbird.boarding.contexts.DevContext.EXISTENT_BOARDING_PASSENGER_HASH;
+import static ca.ulaval.glo4002.thunderbird.boarding.rest.RestTestConfig.buildUrl;
 import static ca.ulaval.glo4002.thunderbird.boarding.rest.RestTestConfig.givenBaseRequest;
-import static junit.framework.TestCase.assertNotNull;
 import static org.eclipse.jetty.http.HttpStatus.Code.*;
-import static org.junit.Assert.assertNotEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.*;
 
 public class SeatAssignationsResourceRestTest {
-    private static final UUID EXISTENT_PASSENGER_HASH = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
     private static final UUID NON_EXISTENT_PASSENGER_HASH = UUID.randomUUID();
     private static final String VALID_MODE = "RANDOM";
     private static final String INVALID_MODE = "INVALID";
@@ -31,11 +26,9 @@ public class SeatAssignationsResourceRestTest {
         seatAssignationRequest = new SeatAssignationRequest();
     }
 
-    @Ignore
     @Test
     public void givenAValidPassengerHashAndAValidMode_whenAssigningSeat_shouldAssignSeat() {
-        //Should use passengerHash based on passenger from boarding created by DevContext of boarding -> we still call it from Reservation here
-        seatAssignationRequest.passengerHash = EXISTENT_PASSENGER_HASH;
+        seatAssignationRequest.passengerHash = EXISTENT_BOARDING_PASSENGER_HASH;
         seatAssignationRequest.mode = VALID_MODE;
 
         Response response;
@@ -48,12 +41,21 @@ public class SeatAssignationsResourceRestTest {
                 .extract()
                 .response();
 
+        Boolean locationValidity = isLocationValid(response.getHeader("Location"));
+        assertTrue(locationValidity);
         String seat = response.path("seat");
         assertNotNull(seat);
         assertNotEquals("", seat);
     }
 
-    @Ignore
+    private boolean isLocationValid(String location) {
+        String baseUrl = buildUrl(SeatAssignationsResource.PATH);
+        baseUrl = baseUrl.replace("/", "\\/");
+        Pattern pattern = Pattern.compile(baseUrl + "\\d+$");
+
+        return pattern.matcher(location).matches();
+    }
+
     @Test
     public void givenAnInvalidPassengerHashAndAValidMode_whenAssigningSeat_shouldReturnNotFound() {
         seatAssignationRequest.passengerHash = NON_EXISTENT_PASSENGER_HASH;
@@ -67,10 +69,9 @@ public class SeatAssignationsResourceRestTest {
                 .statusCode(NOT_FOUND.getCode());
     }
 
-    @Ignore
     @Test
     public void givenAValidPassengerHashAndInvalidMode_whenAssigningSeat_shouldReturnBadRequest() {
-        seatAssignationRequest.passengerHash = EXISTENT_PASSENGER_HASH;
+        seatAssignationRequest.passengerHash = EXISTENT_BOARDING_PASSENGER_HASH;
         seatAssignationRequest.mode = INVALID_MODE;
 
         givenBaseRequest()
@@ -84,10 +85,8 @@ public class SeatAssignationsResourceRestTest {
     @Ignore
     @Test
     public void givenNoMoreSeats_whenAssigningSeat_shouldReturnBadRequest() {
-        seatAssignationRequest.passengerHash = EXISTENT_PASSENGER_HASH;
+        seatAssignationRequest.passengerHash = EXISTENT_BOARDING_PASSENGER_HASH;
         seatAssignationRequest.mode = VALID_MODE;
-        Flight flightMock = mock(Flight.class);
-        Mockito.when(flightMock.assignSeat(any(SeatAssignationStrategy.class))).thenThrow(new SeatNotAvailableException(""));
 
         givenBaseRequest()
                 .body(seatAssignationRequest)
