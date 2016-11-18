c098afbbe4c0d697ef5369645c6b6e1aec35a7eb
Antoine Beaulieu Lessard <antoine222_@hotmail.com>
2016-11-18 13:23:38 -0500

    Added missing annotations

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/BaggageDTO.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/BaggageDTO.java
@@ -1,7 +1,12 @@
 package ca.ulaval.glo4002.thunderbird.boarding.rest.baggage;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class BaggageDTO {
+    @JsonProperty("weight")
     public int weight;
+    @JsonProperty("linear_dimension")
     public int linear_dimension;
+    @JsonProperty("price")
     public float price;
 }
