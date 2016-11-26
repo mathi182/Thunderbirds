96b36516616ffa8e7e0ff18eea2e9bde7eab2165
alexbrillant <abrillant23@gmail.com>
2016-11-26 04:17:28 -0500

    factory test

---
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/domain/passenger/seatClassBaggageStrategy/SeatClassBaggageStrategyFactoryTest.java
@@ -0,0 +1,31 @@
+package ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.seatClassBaggageStrategy;
+
+import ca.ulaval.glo4002.thunderbird.boarding.domain.Seat.Seat;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.*;
+
+public class SeatClassBaggageStrategyFactoryTest {
+    private SeatClassBaggageStrategyFactory factory;
+
+    @Before
+    public void setUp() throws Exception {
+        factory = new SeatClassBaggageStrategyFactory();
+    }
+
+    @Test
+    public void whenGetttingSeatClassCheckedStrategy_shouldReturnEconomySeatClassBaggageStrategy() throws Exception {
+        SeatClassBaggageTemplate strategy = factory.getStrategy(Seat.SeatClass.ECONOMY);
+
+        assertThat(strategy, instanceOf(EconomySeatClassBaggageStrategy.class));
+    }
+
+    @Test
+    public void whenGetttingSeatClassBusinessStrategy_shouldReturnBusinessSeatClassBaggageStrategy() throws Exception {
+        SeatClassBaggageTemplate strategy = factory.getStrategy(Seat.SeatClass.BUSINESS);
+
+        assertThat(strategy, instanceOf(BusinessSeatClassBaggageStrategy.class));
+    }
+}
