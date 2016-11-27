4cf69baba87114f83d1d798d174b900c6813e19d
Jean-Alexandre Beaumont <jean-alexandre.beaumont.1@ulaval.ca>
2016-11-27 14:24:01 -0500

    Added ApplicationService for SeatAssignation.

---
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/application/seat/SeatAssignationApplicationService.java
@@ -0,0 +1,42 @@
+package ca.ulaval.glo4002.thunderbird.boarding.application.seat;
+
+import ca.ulaval.glo4002.thunderbird.boarding.application.ServiceLocator;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.flight.Flight;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.flight.FlightRepository;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.Passenger;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.plane.Seat;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.seatAssignations.SeatAssignationStrategy;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.seatAssignations.SeatAssignationStrategyFactory;
+import ca.ulaval.glo4002.thunderbird.boarding.rest.seatAssignations.SeatAssignationRequest;
+import ca.ulaval.glo4002.thunderbird.boarding.rest.seatAssignations.SeatAssignationRequestAssembler;
+
+
+public class SeatAssignationApplicationService {
+
+    private FlightRepository repository;
+
+    public SeatAssignationApplicationService() {
+        this.repository = ServiceLocator.resolve(FlightRepository.class);
+    }
+
+    public Seat assignSeat(SeatAssignationRequest request) {
+
+        Flight flight = getFlight(request);
+        SeatAssignationStrategy strategy = getSeatAssignationStrategy(request);
+        Seat seat = flight.assignSeat(strategy);
+        seat.take();
+        return seat;
+    }
+
+    private Flight getFlight(SeatAssignationRequest request) {
+        SeatAssignationRequestAssembler seatAssignationRequestAssembler = new SeatAssignationRequestAssembler();
+        Passenger passenger = seatAssignationRequestAssembler.getDomainPassenger(request);
+        return repository.getFlight(passenger.getFlightNumber(), passenger.getFlightDate());
+    }
+
+    private SeatAssignationStrategy getSeatAssignationStrategy(SeatAssignationRequest request) {
+        SeatAssignationRequestAssembler seatAssignationRequestAssembler = new SeatAssignationRequestAssembler();
+        SeatAssignationStrategy.AssignMode assignMode = seatAssignationRequestAssembler.getMode(request);
+        return new SeatAssignationStrategyFactory().getStrategy(assignMode, request.seatClass);
+    }
+}
