b5cae3270baa9980bd828617c26232362baf9dcc
alexbrillant <abrillant23@gmail.com>
2016-11-29 19:05:43 -0500

    PassengerNotCheckedInExceptionMapper

---
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/exceptions/PassengerNotCheckedInExceptionMapper.java
@@ -0,0 +1,15 @@
+package ca.ulaval.glo4002.thunderbird.boarding.rest.baggage.exceptions;
+
+import ca.ulaval.glo4002.thunderbird.boarding.application.baggage.exceptions.PassengerNotCheckedInException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import static org.eclipse.jetty.http.HttpStatus.Code.BAD_REQUEST;
+
+public class PassengerNotCheckedInExceptionMapper implements ExceptionMapper<PassengerNotCheckedInException> {
+    @Override
+    public Response toResponse(PassengerNotCheckedInException e) {
+        return Response.status(BAD_REQUEST.getCode()).build();
+    }
+}
