5f61090bc02836fbf94a1c68394b3ddc01db3d90
Maxime Trottier <maxime.trottier@outlook.com>
2016-12-03 16:04:43 -0500

    Renaming in CheckedBaggage

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/domain/baggage/checked/CheckedBaggage.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/domain/baggage/checked/CheckedBaggage.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Entity
 public class CheckedBaggage extends Baggage {
     private static final int BAGGAGE_COST = 50;
-    private static final float EXCESS_BAGGAGE_FEES = 1.1f;
+    private static final float OVERWEIGHT_BAGGAGE_FEES = 1.1f;
     private static final float SPORT_BAGGAGE_FEES = 1.25f;
 
     public CheckedBaggage(UUID baggageHash, Length linearDimension, Mass weight, String type) {
@@ -28,13 +28,13 @@ public class CheckedBaggage extends Baggage {
 
     @Override
     public float getBasePrice(Length maximumLinearDimension, Mass maximumWeight) {
-        float cost = isSport() ? BAGGAGE_COST * SPORT_BAGGAGE_FEES : BAGGAGE_COST;
+        float cost = isSportType() ? BAGGAGE_COST * SPORT_BAGGAGE_FEES : BAGGAGE_COST;
 
         if (weight.isSuperiorTo(maximumWeight)) {
-            cost *= EXCESS_BAGGAGE_FEES;
+            cost *= OVERWEIGHT_BAGGAGE_FEES;
         }
-        if (!isSport() && linearDimension.isSuperiorTo(maximumLinearDimension)) {
-            cost *= EXCESS_BAGGAGE_FEES;
+        if (!isSportType() && linearDimension.isSuperiorTo(maximumLinearDimension)) {
+            cost *= OVERWEIGHT_BAGGAGE_FEES;
         }
         return cost;
     }
@@ -44,7 +44,7 @@ public class CheckedBaggage extends Baggage {
         return true;
     }
 
-    private boolean isSport() {
+    private boolean isSportType() {
         return Objects.equals(getType(), "sport");
     }
 }
