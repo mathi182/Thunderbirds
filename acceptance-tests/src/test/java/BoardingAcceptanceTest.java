d906d9417965b4c5b460d6bdf35ae81b3b7c1216
alexbrillant <abrillant23@gmail.com>
2016-12-03 17:50:50 -0500

    removed unnecessery test class and directory

--- acceptance-tests/src/test/java/BoardingAcceptanceTest.java
+++ acceptance-tests/src/test/java/BoardingAcceptanceTest.java
@@ -3,6 +3,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "classpath:medium_features/")
-public class RegisterBaggageAcceptanceTest {
+@CucumberOptions(plugin = {"pretty"}, features = "classpath:features/")
+public class BoardingAcceptanceTest {
 }
--- acceptance-tests/src/test/java/SeatAssignmentAcceptanceTest.java
+++
@@ -1,8 +0,0 @@
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "classpath:large_features/")
-public class SeatAssignmentAcceptanceTest {
-}
