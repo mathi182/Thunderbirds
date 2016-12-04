5710ca339a580cacd900c751fd539e4b7872b902
Alexis Lessard <AlexisLessard95@gmail.com>
2016-12-03 21:07:47 -0500

    created AcceptanceContext

---
+++ acceptance-tests/src/test/java/contexts/AcceptanceContext.java
@@ -0,0 +1,15 @@
+package contexts;
+
+import ca.ulaval.glo4002.thunderbird.boarding.application.ServiceLocator;
+import ca.ulaval.glo4002.thunderbird.boarding.application.passenger.PassengerService;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.PassengerRepository;
+import ca.ulaval.glo4002.thunderbird.boarding.persistence.passenger.HibernatePassengerRepository;
+
+public class AcceptanceContext implements context{
+
+    @Override
+    public void apply(){
+        ServiceLocator.reset();
+        ServiceLocator.registerSingleton(PassengerRepository.class,new HibernatePassengerRepository(new PassengerService()));
+    }
+}
---
+++ acceptance-tests/src/test/java/contexts/context.java
@@ -0,0 +1,8 @@
+package contexts;
+
+/**
+ * Created by AlexisLessard on 2016-12-03.
+ */
+public interface context {
+    void apply();
+}
