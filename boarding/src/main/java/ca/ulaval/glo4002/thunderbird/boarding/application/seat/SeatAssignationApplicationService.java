24c232110e49da07cb7ba483c0b1d1833b155b47
Antoine Beaulieu Lessard <litelite@users.noreply.github.com>
2016-12-05 16:05:57 -0500

    Moved enum from strategy to Factory (#101)

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/application/seat/SeatAssignationApplicationService.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/application/seat/SeatAssignationApplicationService.java
@@ -4,6 +4,7 @@ import ca.ulaval.glo4002.thunderbird.boarding.application.ServiceLocator;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.flight.Flight;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.flight.FlightRepository;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.Passenger;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.PassengerRepository;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.plane.Seat;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.seatAssignations.SeatAssignationStrategy;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.seatAssignations.SeatAssignationStrategyFactory;
@@ -12,15 +13,17 @@ import ca.ulaval.glo4002.thunderbird.boarding.rest.seatAssignations.SeatAssignat
 
 public class SeatAssignationApplicationService {
     private final SeatAssignationAssembler seatAssignationRequestAssembler;
-    private final FlightRepository repository;
+    private final FlightRepository flightRepository;
+    private final PassengerRepository passengerRepository;
 
     public SeatAssignationApplicationService() {
         this.seatAssignationRequestAssembler = new SeatAssignationAssembler();
-        this.repository = ServiceLocator.resolve(FlightRepository.class);
+        this.flightRepository = ServiceLocator.resolve(FlightRepository.class);
+        this.passengerRepository = ServiceLocator.resolve(PassengerRepository.class);
     }
 
     public Seat assignSeat(SeatAssignationDTO request) {
-        Passenger passenger = seatAssignationRequestAssembler.getDomainPassenger(request);
+        Passenger passenger = passengerRepository.findByPassengerHash(request.passengerHash);
 
         Flight flight = getFlight(passenger);
         SeatAssignationStrategy strategy = getSeatAssignationStrategy(request, passenger);
@@ -30,11 +33,11 @@ public class SeatAssignationApplicationService {
     }
 
     private Flight getFlight(Passenger passenger) {
-        return repository.getFlight(passenger.getFlightNumber(), passenger.getFlightDate());
+        return flightRepository.getFlight(passenger.getFlightNumber(), passenger.getFlightDate());
     }
 
     private SeatAssignationStrategy getSeatAssignationStrategy(SeatAssignationDTO request, Passenger passenger) {
-        SeatAssignationStrategy.AssignMode assignMode = seatAssignationRequestAssembler.getMode(request);
+        SeatAssignationStrategyFactory.AssignMode assignMode = seatAssignationRequestAssembler.getMode(request);
         return new SeatAssignationStrategyFactory().getStrategy(assignMode, passenger.getSeatClass());
     }
 }
