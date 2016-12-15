a8d9690ee8094b330b457ddf24313c9678084660
Antoine Beaulieu Lessard <antoine222_@hotmail.com>
2016-12-15 14:05:26 -0500

    Added error message

--- boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/exceptions/NoMoreSpaceInFlightExceptionMapper.java
+++ boarding/src/main/java/ca/ulaval/glo4002/thunderbird/boarding/rest/baggage/exceptions/NoMoreSpaceInFlightExceptionMapper.java
@@ -1,17 +1,19 @@
 package ca.ulaval.glo4002.thunderbird.boarding.rest.baggage.exceptions;
 
 import ca.ulaval.glo4002.thunderbird.boarding.application.baggage.exceptions.NoMoreSpaceInFlightException;
+import ca.ulaval.glo4002.thunderbird.boarding.rest.baggage.RegisterBaggageResponse;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import static org.eclipse.jetty.http.HttpStatus.Code.BAD_REQUEST;
+import static org.eclipse.jetty.http.HttpStatus.Code.OK;
 
 @Provider
 public class NoMoreSpaceInFlightExceptionMapper implements ExceptionMapper<NoMoreSpaceInFlightException> {
     @Override
     public Response toResponse(NoMoreSpaceInFlightException noMoreSpaceInFlightException) {
-        return Response.status(BAD_REQUEST.getCode()).build();
+        RegisterBaggageResponse registerBaggageResponse = RegisterBaggageResponse.refused("No more space in plane");
+        return Response.status(OK.getCode()).entity(registerBaggageResponse).build();
     }
 }
