09de7c18fd1dccc7d04359e910585cdd26a847e0
Jean-Alexandre Beaumont <jean-alexandre.beaumont.1@ulaval.ca>
2016-12-20 17:17:56 -0500

    Fatigant. MAIS_T'AVAIS_RAISON.

--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/services/PlaneServiceGlo3000BorderTest.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/services/PlaneServiceGlo3000BorderTest.java
@@ -11,14 +11,14 @@ import static org.junit.Assert.assertNotNull;
 
 public class PlaneServiceGlo3000BorderTest {
 
-    private String validPlaneModel = "dash-8";
-    private String invalidPlaneModel = "asimfsa";
+    private static final String VALID_PLANE_MODEL = "dash-8";
+    private static final String INVALID_PLANE_MODEL = "asimfsa";
 
     private PlaneService planeService = new PlaneServiceGlo3000();
 
     @Test
     public void givenAValidPlaneId_whenGettingPlaneFromService_shouldObtainAPlane() {
-        PlaneId validPlaneId = new PlaneId(validPlaneModel);
+        PlaneId validPlaneId = new PlaneId(VALID_PLANE_MODEL);
 
         Plane actualPlane = planeService.getPlane(validPlaneId);
 
@@ -27,7 +27,7 @@ public class PlaneServiceGlo3000BorderTest {
 
     @Test(expected = PlaneNotFoundException.class)
     public void givenAnInvalidPlaneId_whenGettingPlaneFromService_shouldThrowAnException() {
-        PlaneId invalidPlaneId = new PlaneId(invalidPlaneModel);
+        PlaneId invalidPlaneId = new PlaneId(INVALID_PLANE_MODEL);
 
         planeService.getPlane(invalidPlaneId);
     }
