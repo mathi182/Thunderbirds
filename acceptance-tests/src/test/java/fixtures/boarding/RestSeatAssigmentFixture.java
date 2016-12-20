7eb8abcb77de9c159b2c7e2e7585f962d772e49f
alexbrillant <abrillant23@gmail.com>
2016-12-19 23:32:44 -0500

    getSeatAssignationRequestMap

--- acceptance-tests/src/test/java/fixtures/boarding/RestSeatAssigmentFixture.java
+++ acceptance-tests/src/test/java/fixtures/boarding/RestSeatAssigmentFixture.java
@@ -17,12 +17,12 @@ public class RestSeatAssigmentFixture extends BoardingBaseRestFixture {
 
     public void whenAssignSeat(UUID passengerHash, String mode) {
         URI uri = UriBuilder.fromPath(SeatAssignationsResource.PATH).build();
-        Map<String, Object> body = getJsonAsMap(passengerHash, mode);
+        Map<String, Object> body = getSeatAssignationRequestMap(passengerHash, mode);
         currentRequest = givenRequest().body(body)
                 .when().post(uri);
     }
 
-    private Map<String, Object> getJsonAsMap(UUID passengerHash, String mode) {
+    private Map<String, Object> getSeatAssignationRequestMap(UUID passengerHash, String mode) {
         Map<String, Object> jsonAsMap = new HashMap<>();
         jsonAsMap.put("passenger_hash", passengerHash.toString());
         jsonAsMap.put("mode", mode);
