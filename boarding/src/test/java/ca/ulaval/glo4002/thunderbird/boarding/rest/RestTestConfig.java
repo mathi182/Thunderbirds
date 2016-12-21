d7c55759861ccd140b12946e284d62cdf41f2e52
Alexis Lessard <alexislessard95@gmail.com>
2016-12-20 20:19:40 -0500

    changed port numbers to APPROPRIATE ONES!

--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/RestTestConfig.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/RestTestConfig.java
@@ -6,8 +6,8 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
 
 public final class RestTestConfig {
-    public static final int TEST_SERVER_PORT_BOARDING = 9292;
-    public static final int TEST_SERVER_PORT_RESERVATION = 9293;
+    public static final int TEST_SERVER_PORT_BOARDING = 49155;
+    public static final int TEST_SERVER_PORT_RESERVATION = 50550;
 
     public static String buildUrl(String path) {
         return String.format("http://localhost:%d%s", TEST_SERVER_PORT_BOARDING, path);
