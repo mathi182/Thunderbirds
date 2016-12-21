c4058d5a11ed66c4d5adec82a0ec1351f62e9aed
Jean-Alexandre Beaumont <jean-alexandre.beaumont.1@ulaval.ca>
2016-12-20 19:57:52 -0500

    Added CalculateMass Test

--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/domain/baggage/collection/PassengerBaggageCollectionsTest.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/domain/baggage/collection/PassengerBaggageCollectionsTest.java
@@ -2,8 +2,11 @@ package ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.collection;
 
 import ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.Baggage;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.MedicalBaggage;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.PersonalBaggage;
+import ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.checked.BusinessBaggage;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.baggage.speciality.Speciality;
 import ca.ulaval.glo4002.thunderbird.boarding.domain.passenger.Passenger;
+import ca.ulaval.glo4002.thunderbird.boarding.util.units.Mass;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,8 +20,14 @@ import static org.mockito.Mockito.mock;
 public class PassengerBaggageCollectionsTest {
 
     private static final UUID PASSENGER_UUID = UUID.randomUUID();
+    public static final int PERSO_WEIGHT = 8;
+    public static final int BUSINESS_WEIGHT = 45;
+    public static final int MEDICAL_WEIGHT = 3;
     private PassengerBaggageCollections collections;
     private Baggage baggage;
+    private Baggage personalBaggage;
+    private Baggage businessBaggage;
+    private Mass expectedMass = Mass.fromKilograms(MEDICAL_WEIGHT + BUSINESS_WEIGHT + PERSO_WEIGHT);
 
 
     @Before
@@ -26,6 +35,11 @@ public class PassengerBaggageCollectionsTest {
         Passenger passenger = mock(Passenger.class);
         collections = new PassengerBaggageCollections(passenger);
         baggage = mock(MedicalBaggage.class);
+        personalBaggage = mock(PersonalBaggage.class);
+        businessBaggage = mock(BusinessBaggage.class);
+        willReturn(Mass.fromKilograms(PERSO_WEIGHT)).given(personalBaggage).getWeight();
+        willReturn(Mass.fromKilograms(BUSINESS_WEIGHT)).given(businessBaggage).getWeight();
+        willReturn(Mass.fromKilograms(MEDICAL_WEIGHT)).given(baggage).getWeight();
         willReturn(false).given(baggage).hasSpeciality(any(Speciality.class));
     }
 
@@ -51,5 +65,16 @@ public class PassengerBaggageCollectionsTest {
 
         int actualSize = collections.collection.size();
         assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void givenACollectionsWithThreeBaggages_whenCalculatingMass_shouldReturnCorrectMass() {
+        collections.addBaggage(baggage);
+        collections.addBaggage(businessBaggage);
+        collections.addBaggage(personalBaggage);
+
+        Mass actualMass = collections.calculateBaggagesMass();
+
+        assertEquals(expectedMass,actualMass);
     }
 }
