9ac8d046ddbb9ae17edf3b54cb10f56fb5806fe0
alexbrillant <abrillant23@gmail.com>
2016-12-20 20:16:24 -0500

    system ports in server

--- boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/RestTestConfig.java
+++ boarding/src/test/java/ca/ulaval/glo4002/thunderbird/boarding/rest/RestTestConfig.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 
 public final class RestTestConfig {
     public static final int TEST_SERVER_PORT_BOARDING = 9292;
-    public static final int TEST_SERVER_PORT_RESERVATION = 8787;
+    public static final int TEST_SERVER_PORT_RESERVATION = 9293;
 
     public static String buildUrl(String path) {
         return String.format("http://localhost:%d%s", TEST_SERVER_PORT_BOARDING, path);
